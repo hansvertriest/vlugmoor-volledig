7f45c39abf6c6474e97ddde37c3ec314dc48012c
Arne Verleyen <ArneVerleyen@MacBook-Pro-van-Arne.local>
2020-12-14 17:05:50 +0100

    frontend updates: dom genereren van data

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -4,14 +4,41 @@ import ApiService from '../lib/api/ApiService';
 const homeTemplate = require('../templates/home.hbs');
 
 export default () => {
+    const title = 'Home page';
+    App.render(homeTemplate({title}));
+
+    let documentContainer = document.getElementById('container-home');
+    
+    const showMetaData = (metaData) => {
+        metaData.forEach(data => {
+            console.log(data.description); 
+    
+            let container = document.createElement('div');
+            let image = document.createElement('img');
+            let title = document.createElement('h3');
+            let description = document.createElement('p');
+
+            container.setAttribute('class', 'card col-12 col-sm-12 col-md-6 col-lg-4');
+            
+            documentContainer.appendChild(container);
+            container.appendChild(title);
+            container.appendChild(description);
+
+            title.innerHTML = data.title;
+            description.innerHTML = data.description;
+            
+            documentContainer.appendChild(container);
+        })
+    }
+
+
     const apiService = new ApiService;
     const data = apiService.findAllMetaData();
     data.then(
-        metaData => console.log(metaData)
+        metaData => showMetaData(metaData)
     );
 
     
 
-    const title = 'Home page';
-    App.render(homeTemplate({title}));
+
 };
